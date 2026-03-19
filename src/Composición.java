6b9a3700f7b2f185cb48ef93cc5247caf6f4330d
antony <antonioseguracastillo96@gmail.com>
2026-03-19 08:00:20 -0600

    Update

--- src/Composición.java
+++ src/Composición.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
 public class Composición {
+    private String titulo;
+    private int duracionMin;
+    private int duracionSeg;
+    private String genero;
+    private String fechaRegistro;
+    private String fechaEstreno;
+
+    private ArrayList<String> interpretes;
+    private ArrayList<Solicitud> solicitudes;
+
+
+    public Composición(String titulo, int duracionMin, int duracionSeg,
+                       String genero, String fechaRegistro, String fechaEstreno) {
+        this.titulo = titulo;
+        this.duracionMin = duracionMin;
+        this.duracionSeg = duracionSeg;
+        this.genero = genero;
+        this.fechaRegistro = fechaRegistro;
+        this.fechaEstreno = fechaEstreno;
+
+
+        interpretes = new ArrayList<>();
+        solicitudes = new ArrayList<>();
+
+
+    }
+    public void agregarInterprete(String nombre){
+        interpretes.add(nombre);
+    }
+    public void agregarSolicitud(Solicitud s){
+        solicitudes.add(s);
+        interpretes.add(s.getNombreInterprete());
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getDuracionMin() {
+        return duracionMin;
+    }
+
+    public int getDuracionSeg() {
+        return duracionSeg;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public String getFechaEstreno() {
+        return fechaEstreno;
+    }
+    @Override
+    public String toString() {
+        return "Composición{" +
+                "titulo='" + titulo + '\'' +
+                ", duracionMin=" + duracionMin +
+                ", duracionSeg=" + duracionSeg +
+                ", genero='" + genero + '\'' +
+                ", fechaRegistro='" + fechaRegistro + '\'' +
+                ", fechaEstreno='" + fechaEstreno + '\'' +
+                '}';
+    }
 }
--- src/Solicitud.java
+++ src/Solicitud.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
-
 public class Solicitud {
+    private String nombreInterprete;
+    private String fechaSolicitud;
+    private String fechaAutorizacion;
+    private String companiaGrabacion;
+
+    public Solicitud(String nombreInterprete, String fechaSolicitud, String fechaAutorizacion, String companiaGrabacion) {
+        this.nombreInterprete = nombreInterprete;
+        this.fechaSolicitud = fechaSolicitud;
+        this.fechaAutorizacion = fechaAutorizacion;
+        this.companiaGrabacion = companiaGrabacion;
+    }
+
+    public String getNombreInterprete() {
+        return nombreInterprete;
+    }
+
+    public String getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+
+    public String getFechaAutorizacion() {
+        return fechaAutorizacion;
+    }
+
+
+    public void setNombreInterprete(String nombreInterprete) {
+        this.nombreInterprete = nombreInterprete;
+    }
+
+    public void setFechaSolicitud(String fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+
+    public void setFechaAutorizacion(String fechaAutorizacion) {
+        this.fechaAutorizacion = fechaAutorizacion;
+    }
+
+    public void setCompaniaGrabacion(String companiaGrabacion) {
+        this.companiaGrabacion = companiaGrabacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Solicitud{" +
+                "nombreInterprete='" + nombreInterprete + '\'' +
+                ", fechaSolicitud='" + fechaSolicitud + '\'' +
+                ", fechaAutorizacion='" + fechaAutorizacion + '\'' +
+                ", companiaGrabacion='" + companiaGrabacion + '\'' +
+                '}';
+    }
 }
